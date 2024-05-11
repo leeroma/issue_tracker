320ee008c3b3560516192c4f50a53eab13a5204e
Roma <roma.li0071@gmail.com>
2024-05-11 17:27:17 +0500

    added models, added models into admin, made migrations

--- issuetracker/admin.py
+++ issuetracker/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from issuetracker.models import Issue, Status, Type
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('id', 'summary',)
+    list_filter = ('type', 'status',)
+    search_fields = ('id', 'summary', 'description',)
+    exclude = []
+    readonly_fields = ('created_at', 'updated_at',)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('id', 'name',)
+    exclude = []
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('id', 'name',)
+    exclude = []
+
+
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Type, TypeAdmin)
--- issuetracker/models.py
+++ issuetracker/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Issue(models.Model):
+    summary = models.CharField('Заголовок', max_length=100, null=False, blank=False)
+    description = models.CharField('Описание', max_length=3000, null=True, blank=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, related_name='status', verbose_name='Статус')
+    type = models.ForeignKey(Status, on_delete=models.PROTECT, related_name='type', verbose_name='Тип')
+
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата обновления', auto_now=True)
+
+    def __str__(self):
+        return self.id, self.summary
