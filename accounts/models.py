7133dfce1ff00199166252751a79fce1eee78211
Roma <roma.li0071@gmail.com>
2024-05-28 20:47:27 +0500

    created user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Account(AbstractUser):
+    email = models.EmailField('Адрес электронной почты', unique=True, blank=False, null=False)
+    first_name = models.CharField('Имя', max_length=100)
+    last_name = models.CharField('Фамилия', max_length=100)
